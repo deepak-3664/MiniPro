12a7dbe33404b792629ee782d4195a1dc57eaf7f
deepak-3664 <113664553+deepak-3664@users.noreply.github.com>
2023-07-13 09:21:37 +0530

    Add files via upload

---
+++ Day6/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
+import Home from "./Home";
+import Signup from "./pages/login/Signup";
+import Login from "./pages/login/login";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/Signup" element={<Signup />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+export default App;
