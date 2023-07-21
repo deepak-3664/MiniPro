cb3e970f3151e76731bfeb3a63226be43dc9849a
deepak-3664 <113664553+deepak-3664@users.noreply.github.com>
2023-07-21 09:22:34 +0530

    Add files via upload

---
+++ Day-9/pages/AboutUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AboutUs.css';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+        
+      <h1>About Us</h1>
+      <p>Welcome to our company! We are a passionate team dedicated to delivering exceptional products and services to our customers.</p>
+      <p>Our mission is to provide innovative solutions that solve real-world problems and improve people's lives.</p>
+      <p>Read on to learn more about our team and what we do.</p>
+      {/* Add more content as needed */}
+    </div>
+  );
+};
+
+export default AboutUs;
